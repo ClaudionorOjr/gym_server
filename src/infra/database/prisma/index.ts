e25ada5f6d84e012dbe96b0c95d43d26d557f8c3
Claudionor Oliveira <claudionorojr@hotmail.com>
2024-02-26 16:08:43 -0300

    chore(infra): change .env file, create alpha branch

--- src/infra/database/prisma/index.ts
+++ src/infra/database/prisma/index.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 import { DatabaseProvider } from '../database-provider'
 import { injectable } from 'tsyringe'
+import { env } from '@infra/env'
 
 @injectable()
 export class PrismaService extends PrismaClient implements DatabaseProvider {
   constructor() {
     super({
-      log: ['query', 'warn', 'error'],
+      log:
+        env.NODE_ENV === 'development' ? ['query', 'warn', 'error'] : ['error'],
     })
   }
 
