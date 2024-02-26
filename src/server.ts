af36cba5c661d42660828865d1138716b76c57b6
Claudionor Oliveira <claudionorojr@hotmail.com>
2024-02-26 18:09:33 -0300

    ci: e2e tests jobs changed in ci file
    Added environment variables and postgres database service

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { env } from '@infra/env'
 import { app } from './app'
 
 app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
-  console.log(`Server running on http://localhost:${env.PORT}`)
+  console.log(`Server Running... 🚀`)
 })
 
 // app.server.address()
